34d9841dd82f6960279b9562f85e726f71febeff
Vanshika <vanshikabhojnagarwala@gmail.com>
2023-12-05 18:51:57 +0530

    feat : add hero jumbotron

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,17 @@
 import NavbarComponent from "./Navbar";
 
 const Home = () => {
-  return <div className="home">
-  </div>;
+  return (
+    <div className="home">
+      <div className="absolute top-48 left-52 z-10 border-l-4 border-[#FDBF00] pl-10">
+        <h1 className="text-5xl font-medium text-white">Study Abroad</h1>
+        <h3 className="text-xl text-white my-2">Anything, anywhere with the experts</h3>
+        <button className="px-4 py-2 border-2 bg-[#FDBF00] rounded text-white my-4 text-sm font-medium">
+          Book a consultation
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
